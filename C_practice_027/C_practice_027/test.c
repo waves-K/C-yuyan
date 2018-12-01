fe9083c550938686e0d9b720980a92a775c40ced
waves-K <1505906381@qq.com>
2018-12-01 21:19:08 +0800

    杨辉三角 预测比赛 超级侦探

---
+++ C_practice_027/C_practice_027/test.c
@@ -0,0 +1,277 @@
+//1.
+//5位运动员参加了10米台跳水比赛，有人让他们预测比赛结果
+//A选手说：B第二，我第三；
+//B选手说：我第二，E第四；
+//C选手说：我第一，D第二；
+//D选手说：C最后，我第三；
+//E选手说：我第四，A第一；
+//比赛结束后，每位选手都说对了一半，请编程确定比赛的名次。
+//
+
+
+//#include<stdio.h>
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	int d = 0;
+//	int e = 0;
+//	for (a = 1; a <= 5; a++) {
+//		for (b = 1; b <= 5; b++) {
+//			for (c = 1; c <= 5; c++) {
+//				for (d = 1; d <= 5; d++) {
+//					for (e = 1; e <= 5; e++) {
+//						if (((b == 2) + (a == 3) == 1) &&
+//							((b == 2) + (e == 4) == 1) &&
+//							((c == 1) + (d == 2) == 1) &&
+//							((c == 5) + (d == 3) == 1) &&
+//							((e == 4) + (a == 1) == 1)) {
+//							if(a * b * c * d * e == 120){
+//								printf("a=%d b=%d c=%d d=%d e=%d\n", a, b, c, d, e);
+//							}
+//						}
+//					}
+//				}
+//			}
+//		}
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+
+
+//2.
+//日本某地发生了一件谋杀案，警察通过排查确定杀人凶手必为4个嫌疑犯
+//的一个。以下为4个嫌疑犯的供词。
+//A说：不是我。
+//B说：是C。
+//C说：是D。
+//D说：C在胡说
+//已知3个人说了真话，1个人说的是假话。
+//现在请根据这些信息，写一个程序来确定到底谁是凶手。
+//
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	int killer = 0;
+//	for (killer = 'a'; killer <= 'd'; killer++) {
+//		if ((killer != 'a') + (killer == 'c') + (killer == 'd') + (killer != 'd') == 3) {
+//			printf("%c\n", killer);
+//		}
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+
+//3.在屏幕上打印杨辉三角。
+//
+//    1
+//   1 1
+//  1 2 1
+// 1 3 3 1
+//1 4 5 4 1
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	int arr[10][10] = { 0 };
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < 10; i++) {
+//		for (j = 0; j < 10; j++) {
+//			if (j == 0) {
+//				arr[i][j] = 1;
+//			}
+//			if (i == j) {
+//				arr[i][j] = 1;
+//			}
+//			if ((i > 1) && (j > 0)) {
+//				arr[i][j] = arr[i - 1][j] + arr[i - 1][j - 1];
+//			}
+//		}
+//	}
+//	for (i = 0; i < 10; i++) {
+//		for (j = 0; j <= i; j++) {
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+//4. 总结数组的相关知识，写一篇博客。
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	int d = 0;
+//	int e = 0;
+//	for (a = 0; a <= 5; a++) {
+//		for (b = 0; b <= 5; b++) {
+//			for (c = 0; c <= 5; c++) {
+//				for (d = 0; d <= 5; d++) {
+//					for (e = 0; e <= 5; e++) {
+//						if (((b == 2) + (a == 3) == 1) &&
+//							((b == 2) + (e == 4) == 1) &&
+//							((c == 1) + (d == 2) == 1) &&
+//							((c == 5) + (d == 3) == 1) &&
+//							((e == 4) + (a == 1) == 1)) {
+//							if (a * b * c * d * e == 120) {
+//								printf("a=%d b=%d c=%d d=%d e=%d\n", a, b,
+//									c, d, e);
+//							}
+//						}
+//					}
+//				}
+//			}
+//		}
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	int killer = 0;
+//	for (killer = 'a'; killer <= 'd'; killer++) {
+//		if ((killer != 'a') + (killer == 'c') + (killer == 'd')
+//			+ (killer != 'd') == 3) {
+//			printf("%c\n", killer);
+//		}
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	int arr[10][10] = { 0 };
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < 10; i++) {
+//		for (j = 0; j < 10; j++) {
+//			if (j == 0) {
+//				arr[i][j] = 1;
+//			}
+//			if (j == i) {
+//				arr[i][j] = 1;
+//			}
+//			if ((i > 1) && (j > 0)) {
+//				arr[i][j] = arr[i - 1][j] + arr[i - 1][j - 1];
+//			}
+//		}
+//	}
+//
+//	int k = 0;
+//	for (i = 0; i < 10; i++) {
+//		for (k = i; k < 10 - 1; k++) {
+//			printf("   ");
+//		}
+//		for (j = 0; j <= i; j++) {
+//			printf("%3d   ", arr[i][j]);
+//			
+//		}
+//		printf("\n");
+//		printf("\n");
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	int arr[10][10] = { 0 };
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < 10; i++) {
+//		for (j = 0; j < 10; j++) {
+//			if (j == 0) {
+//				arr[i][j] = 1;
+//			}
+//			if (j == i) {
+//				arr[i][j] = 1;
+//			}
+//			if ((i > 1) && (j > 0)) {
+//				arr[i][j] = arr[i - 1][j] + arr[i - 1][j - 1];
+//			}
+//		}
+//	}
+//	int k = 0;
+//	for (i = 0; i < 10; i++) {
+//		for (k = i; k < 10 - 1; k++) {
+//			printf("   ");
+//		}
+//		for (j = 0; j <= i; j++) {
+//			printf("%3d   ", arr[i][j]);
+//		}
+//		printf("\n");
+//		printf("\n");
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int arr[10][10] = { 0 };
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < 10; i++) {
+		for (j = 0; j < 10; j++) {
+			if (j == 0) {
+				arr[i][j] = 1;
+			}
+			if (j == i) {
+				arr[i][j] = 1;
+			}
+			if ((i > 1) && (j > 0)) {
+				arr[i][j] = arr[i - 1][j] + arr[i - 1][j - 1];
+			}
+		}
+	}
+	int k = 0;
+	for (i = 0; i < 10; i++) {
+		for (k = i; k < 10 - 1; k++) {
+			printf("   ");
+		}
+		for (j = 0; j <= i; j++) {
+			printf("%3d   ", arr[i][j]);
+		}
+		printf("\n");
+		printf("\n");
+	}
+	system("pause");
+	return 0;
+}
